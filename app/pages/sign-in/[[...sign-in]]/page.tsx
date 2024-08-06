cc7bffb524f86ad8658e95309528fce576da3441
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-08-06 15:33:46 +0530

    navbar desktop

--- app/pages/sign-in/[[...sign-in]]/page.tsx
+++ app/pages/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from '@clerk/nextjs'
 const Page = () => {
   return (
     <div className='flex flex-col md:flex-row relative min-h-screen'>
-      <img src='/signup_img.png' className='hidden lg:block h-[725px] rounded' />
+      <img src='/signup_img.png' className='hidden lg:block rounded h-screen' />
       <div className='absolute top-5 left-5 text-2xl font-black italic'>
         VASRA.CO
       </div>
--- app/pages/sign-up/[[...sign-up]]/page.tsx
+++ app/pages/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from '@clerk/nextjs'
 const Page = () => {
   return (
     <div className='flex flex-col md:flex-row relative min-h-screen'>
-      <img src='/login_img.png' className='hidden lg:block h-[725px] rounded' />
+      <img src='/login_img.png' className='hidden lg:block h-screen rounded' />
       <div className='absolute top-5 left-5 text-2xl font-black italic'>
         VASRA.CO
       </div>
