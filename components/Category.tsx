3bccc2391a2bfb5580a9cb9587c30005cbf84d68
Abijeeth Vasra <abijeeth.vasra@bounteous.com>
2024-08-07 18:15:39 +0530

    image tab fix

--- components/Category.tsx
+++ components/Category.tsx
@@ -4,11 +4,11 @@ const Category = () => {
   return (
     <div className='bg-gray-200 rounded-3xl p-10 uppercase flex gap-5 flex-col justify-center items-center m-10'>
         <h1 className='font-black text-4xl '>Browse By Dress Style</h1>
-        <div className='flex flex-col md:flex-row gap-5 mt-10'>
+        <div className='flex flex-col lg:flex-row gap-5 mt-10'>
             <img src="/Casual.png" />
-            <img src="/Formal.png" className='flex'/>
+            <img src="/Formal.png" />
         </div>
-        <div className='flex flex-col md:flex-row gap-5'>
+        <div className='flex flex-col lg:flex-row gap-5'>
             <img src="/Gym.png" />
             <img src="/Party.png" />
         </div>
